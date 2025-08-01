93d4be5a4df8ade309012c76a56049eea8e53f17
nphacks <164674563+nphacks@users.noreply.github.com>
2025-08-01 08:07:40 -0400

    Cors

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:4200"], 
-    allow_origins=["*"],
+    allow_origins=["http://localhost:4200", "https://master.d57r72nn2fla0.amplifyapp.com/"], 
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
